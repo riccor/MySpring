42712b1ec7f97d456452c5bb23d88b1250e34829
riccor <ricardocorreia_6@hotmail.com>
2018-07-07 17:31:57 +0100

    v11

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -23,7 +23,7 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-        System.out.println("hi =) and bye!! and nothing else HMMM v100");
+        System.out.println("hi =) and bye!! and nothing else HMMM v101s");
     }
 }
 
