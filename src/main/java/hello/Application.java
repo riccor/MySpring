0fa7564cf541159d05cac14226edb8a123668349
riccor <ricardocorreia_6@hotmail.com>
2018-07-07 18:03:02 +0100

    v114 (new-branch)

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -23,7 +23,7 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-        System.out.println("hi =) and bye!! and nothing else HMMM v101s - going to shelf - master v112");
+        System.out.println("hi =) and bye!! and nothing else HMMM v101s - going to shelf - master v112 ok");
     }
 }
 
